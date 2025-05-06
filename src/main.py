1af43cc448b9a8a22907dd9bdb1da2afae54b210
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2025-05-05 22:31:26 -0500

    feat: add configuration for cors origin requests

--- src/main.py
+++ src/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8080",
+        "https://kuid-pied.vercel.app/login",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
